7472fa61daf5d94945ddc3a0206f625bb145e61d
Jaime Pineda <jpin730@pm.me>
2026-03-09 16:13:58 -0600

    chore: :wrench: config eslint, prettier, husky and lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,81 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import css from '@eslint/css'
+import js from '@eslint/js'
+import json from '@eslint/json'
+import markdown from '@eslint/markdown'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import importPlugin from 'eslint-plugin-import'
+import prettier from 'eslint-plugin-prettier/recommended'
+import ts from 'typescript-eslint'
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
+    // eslint-disable-next-line import/no-named-as-default-member
+    extends: [js.configs.recommended, ts.configs.recommended, prettier, ...nextVitals, ...nextTs],
+    settings: importPlugin.flatConfigs.typescript.settings,
+    rules: {
+      ...importPlugin.flatConfigs.recommended.rules,
+      ...importPlugin.flatConfigs.typescript.rules,
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
+      ],
+      'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            ['builtin', 'external'],
+            ['internal', 'parent', 'sibling', 'index', 'object', 'type'],
+            'unknown',
+          ],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', orderImportKind: 'asc', caseInsensitive: true },
+          named: { enabled: true, types: 'types-first' },
+          sortTypesGroup: true,
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.css'],
+    language: 'css/css',
+    plugins: { css },
+    extends: ['css/recommended'],
+    languageOptions: {
+      tolerant: true,
+    },
+  },
+  {
+    files: ['**/*.json'],
+    ignores: ['package-lock.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.jsonc'],
+    plugins: { json },
+    language: 'json/jsonc',
+    extends: ['json/recommended'],
+  },
 
-export default eslintConfig;
+  {
+    files: ['**/*.json5'],
+    plugins: { json },
+    language: 'json/json5',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.md'],
+    plugins: {
+      markdown,
+    },
+    extends: ['markdown/recommended'],
+  },
+])
+
+export default eslintConfig
